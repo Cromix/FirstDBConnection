1eeae9092a2644d8a4e96e7509b157116e3b48cc
Cromix <32061386+Cromix@users.noreply.github.com>
2019-03-19 00:19:02 +0200

    add project to git

---
+++ BackEnd/Dashboard/src/main/java/com/cromixsoft/Dashboard/controllers/CityController.java
@@ -0,0 +1,35 @@
+package com.cromixsoft.Dashboard.controllers;
+
+import com.cromixsoft.Dashboard.models.City;
+import com.cromixsoft.Dashboard.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CityController {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @GetMapping("/best")
+    public String test() {
+        return "dfsafs";
+    }
+
+    //
+    @GetMapping("/allUsers")
+    public List<City> allCities() {
+        System.out.println(cityRepository.findById(712));
+        return cityRepository.findAll();
+    }
+
+    @GetMapping("/add")
+    public City addCity() {
+        return cityRepository.save(new City("Zeu", 2333l));
+    }
+
+
+}
